970a902b0e7fa3879d58fd6f95699e41627e1321
yawnypaws <cora@pdx.edu>
2017-07-02 09:30:36 -0700

    Update test1.py

--- Pozyx-Python-library-master/tutorials/test1.py
+++ Pozyx-Python-library-master/tutorials/test1.py
@@ -1,5 +1,6 @@
 import time
 start = time.time()
 print("this is an example to see how editing code looks in github")
+print("will it show that i included this line sine my last save?")
 end = time.time()
 print(end - start)
