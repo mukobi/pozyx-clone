7f6699ff33615c78f00e77a2073146e8f5ddabf2
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-07-07 10:13:27 -0700

    update data_functions with reST documentation

--- house code/data_functions.py
+++ house code/data_functions.py
@@ -3,15 +3,13 @@ def strSetLength(number, length):
     """
     Make a data value have a set character length.
 
+    :param number: the data point, probably a number, that you want to round
+    :param length: the length of characters you want the output to be
+
+    :return numString: a string representation of the input number with set length
+
     This function takes a number and rounds it off/adds zeros to return a string of the number with a set character length
     This is to make it easier to read the data from the console since every row will have the same number of data points
-
-    Keyword arguments:
-    number -- the data point, probably a number, that you want to round
-    length -- the length of characters you want the output to be
-
-    Keyword return:
-    numString -- a string representation of the input number with set length
 
     Ex: strSetLength(352.3549234234, 6) --> 352.35
     strSetLength(23.22, 7) --> 23.2200
