beef7c20cf4ece889fb66c105ba29d0875047345
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-07-07 10:03:08 -0700

    Document strSetLength

--- house code/data_functions.py
+++ house code/data_functions.py
@@ -1,14 +1,22 @@
 
 def strSetLength(number, length):
     """
-    Takes a data value and makes it have a set character length by adding or removing characters at the end.
+    Make a data value have a set character length.
 
-    this function takes a number and rounds it off/adds zeros to return a string of the number with a set character length
-    this is to make it easier to read the data from the console since every row will have the same number of data points
+    This function takes a number and rounds it off/adds zeros to return a string of the number with a set character length
+    This is to make it easier to read the data from the console since every row will have the same number of data points
+
+    Keyword arguments:
+    number -- the data point, probably a number, that you want to round
+    length -- the length of characters you want the output to be
+
+    Keyword return:
+    numString -- a string representation of the input number with set length
+
+    Ex: strSetLength(352.3549234234, 6) --> 352.35
+    strSetLength(23.22, 7) --> 23.2200
     """
-    @params:
     numString = str(number);
-    numLength = len(numString);
     while len(numString) < length:
         numString += "0"
     while len(numString) > length:
