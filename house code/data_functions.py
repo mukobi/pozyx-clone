f944be35af25d34fd5b398e0b25a0a5f49fce267
Glen-Skahill <gskahill@gmail.com>
2017-07-07 10:07:36 -0700

    Added Total Distance and Write to File Functions

--- house code/data_functions.py
+++ house code/data_functions.py
@@ -1,4 +1,23 @@
 
+def findtotaldistance(position, prev_pos, total_distance):
+    '''Function to determine the total distance travelled by the Pozyx device'''
+    """
+    Put in main,
+    total_distance = 0
+    prev_pos = 0
+    Put in while loop,
+    total_distance = findtotaldistance(pos, prev_pos, total_distance)
+    prev_pos = pos
+    """
+    from math import sqrt
+    if prev_pos != 0:
+        temp_dist = sqrt((position.x - prev_pos.x)**2 + (position.y - prev_pos.y)**2 +(position.z - prev_pos.z)**2)
+        total_distance += temp_dist
+    else:
+        temp_dist = sqrt((position.x)**2 + (position.y)**2 +(position.z)**2)
+        total_distance += temp_dist
+    return total_distance
+
 def strSetLength(number, length):
     """
     Takes a data value and makes it have a set character length by adding or removing characters at the end.
