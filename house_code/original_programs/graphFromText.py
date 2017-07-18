f63649511fe7ce80b91d655deccb723ecc3ef86b
yawnypaws <cora@pdx.edu>
2017-07-18 09:15:56 -0700

    graphing from text file

--- house_code/original_programs/graphFromText.py
+++ house_code/original_programs/graphFromText.py
@@ -1,24 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df=pd.read_csv('/Users/CoraJune/Documents/GitHub/Pozyx/Data/FrisbeeTestAltered.txt', header=None, delimiter=' ', usecols=[2,16], names=['Time','Angular Velocity'])
 
-headers = ['X','Y']
-
-
-df=pd.read_csv('/Users/CoraJune/Documents/GitHub/Pozyx/house code/tutorials altered/FrisbeeFast1.txt', header=None, delimiter=' ', usecols=[2,16])
-
-
-ax = df.plot.scatter(x=2, y=16, marker='.',  title='Angular Velocity')
+ax = df.plot.scatter(x='Time', y='Angular Velocity', s=3,  title='Angular Velocity')
 ax.set_xlabel("Time")
 ax.set_ylabel("Angular Velocity")
-
-
-print(df)
+ax.plot(kind='line', subplots=True, grid=True, title="Sample Data (Unit)",
+    layout=(4, 3), sharex=True, sharey=False)
 plt.show()
 
+df1=df.diff(1,0)['Angular Velocity']
+ax1=df1.plot.line(x=2, y='Angular Velocity', linewidth=1, title='Angular Acceleration')
 
-df1=pd.read_csv('/Users/CoraJune/Desktop/HomeData/151g.txt', header=None, delimiter=' ', usecols=[2,16])
-ax1=df1.plot.scatter(x=2, y=16, marker='.', title='Angular Velocity 2')
-
+ax1.set_xlabel("Time")
+ax1.set_ylabel("Angular Acceleration")
+ax1.plot()
 plt.show()
 
