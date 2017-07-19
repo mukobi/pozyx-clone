bd33cadeb558cb8add62ef196daad2fade2b6f3d
yawnypaws <cora@pdx.edu>
2017-07-19 10:33:37 -0700

    graphing linear acceleration and normal acceleration

--- house_code/original_programs/graphFromText2.py
+++ house_code/original_programs/graphFromText2.py
@@ -1,22 +1,67 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+headers=['Time', 'Linear-Acceleration-X']
+
+df=pd.read_csv('/Users/CoraJune/Documents/GitHub/Pozyx/Data/motor_turntable_2.txt', delimiter=' ',  usecols=['Time','Linear-Acceleration-X','Acceleration-X','Linear-Acceleration-Y','Acceleration-Y','Linear-Acceleration-Z','Acceleration-Z'])
+
+x=df['Time']
+y1=df['Linear-Acceleration-X']
+y2=df['Acceleration-X']
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.show()
+
+x=df['Time']
+y3=df['Linear-Acceleration-Y']
+y4=df['Acceleration-Y']
+plt.plot(x,y3)
+plt.plot(x,y4)
+plt.show()
+
+x=df['Time']
+y5=df['Linear-Acceleration-Z']
+y6=df['Acceleration-Z']
+plt.plot(x,y5)
+plt.plot(x,y6)
+plt.show()
 
 
-df=pd.read_csv('/Users/CoraJune/Documents/GitHub/Pozyx/Data/FrisbeeTestAltered.txt', header=None, delimiter=' ', usecols=[2,16], names=['Time','Angular Velocity'])
 
-
-ax = df.plot.line(x='Time', y='Angular Velocity', linewidth=1,  title='Angular Velocity')
+ax = df.plot.scatter(x='Time', y='Linear-Acceleration-X', s=1,  title='Linear-Acceleration-X')
 ax.set_xlabel("Time")
-ax.set_ylabel("Angular Velocity")
+ax.set_ylabel("Acceleration-X")
 ax.plot()
 plt.show()
 
-df1=df.diff(1,0)['Angular Velocity']
-ax1=df1.plot.line(x=2, y='Angular Velocity', linewidth=1, title='Angular Acceleration')
+df=pd.read_csv('/Users/CoraJune/Documents/GitHub/Pozyx/Data/motor_turntable_2.txt', delimiter=' ',  usecols=['Time','Linear-Acceleration-Y'])
 
-ax1.set_xlabel("Time")
-ax1.set_ylabel("Angular Acceleration")
-ax1.plot()
+
+ax1 = df.plot.scatter(x='Time', y='Linear-Acceleration-Y', s=1,  title='Linear-Acceleration-Y')
+ax.set_xlabel("Time")
+ax.set_ylabel("Acceleration-Y")
+ax.plot()
 plt.show()
+
+df=pd.read_csv('/Users/CoraJune/Documents/GitHub/Pozyx/Data/motor_turntable_2.txt', delimiter=' ',  usecols=['Time','Linear-Acceleration-Z'])
+
+
+ax2 = df.plot.scatter(x='Time', y='Linear-Acceleration-Z', s=1,  title='Linear-Acceleration-Z')
+ax2.set_xlabel("Time")
+ax2.set_ylabel("Acceleration-Z")
+ax2.plot()
+plt.show()
+
+df.plot()
+
+
+
+#df1=df.diff(1,0)['Angular Velocity']
+#ax1=df1.plot.line(x=2, y='Angular Velocity', linewidth=1, title='Angular Acceleration')
+
+#ax1.set_xlabel("Time")
+#ax1.set_ylabel("Angular Acceleration")
+#ax1.plot()
+#plt.show()
+
 
