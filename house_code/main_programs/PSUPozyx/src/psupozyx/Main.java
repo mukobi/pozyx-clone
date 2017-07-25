e6a71cf4f2600b07a0eab7b21e2a2ad18c88339f
Gabe Mukobi <gabrielmukobi@gmail.com>
2017-07-24 19:29:51 -0700

    Shrink UI

--- house_code/main_programs/PSUPozyx/src/psupozyx/Main.java
+++ house_code/main_programs/PSUPozyx/src/psupozyx/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("layout_main.fxml"));
         primaryStage.setTitle("PSU Pozyx");
-        primaryStage.setScene(new Scene(root, 1280, 800));
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
