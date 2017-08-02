09cce159e489df1312b6004f6cea85a33b50f294
Glen-Skahill <gskahill@gmail.com>
2017-08-02 15:18:11 -0700

    Velocity Class and Implementation
    Velocity is now a class that has been implemented in both 3D positioning files. It successfully logs the velocity to console and write the velocity in a nice format to a csv file for future data analysis.
    Functionality of switching on and off velocity calculations will later be implemented.

--- house_code/main_programs/PSUPozyx/modules/data_functions.py
+++ house_code/main_programs/PSUPozyx/modules/data_functions.py
@@ -147,3 +147,59 @@ class DataFunctions:
             print("Invalid input, bin size set to 10 by default.")
             bin_input = 10
         return bin_input
+
+class Velocity:
+    """
+    This class is to be used for the calculation of velocity on the X, Y and Z axes.
+    """
+    @staticmethod
+    def update_bins(bin_pos_x, bin_pos_y, bin_pos_z, bin_time, elapsed, one_cycle_position):
+        from modules.data_averaging import BinData as BinData
+
+        BinData.add(bin_pos_x, one_cycle_position.x)         #creating a list of x position data points for calculation
+        binned_pos_x = BinData.return_data(bin_pos_x)         #getting that list
+
+        BinData.add(bin_pos_y, one_cycle_position.y)         #creating a list of x position data points for calculation
+        binned_pos_y = BinData.return_data(bin_pos_y)
+
+        BinData.add(bin_pos_z, one_cycle_position.z)         #creating a list of x position data points for calculation
+        binned_pos_z = BinData.return_data(bin_pos_z)
+
+        BinData.add(bin_time, elapsed)
+        binned_time = BinData.return_data(bin_time)
+
+        return binned_pos_x, binned_pos_y, binned_pos_z, binned_time
+
+    @staticmethod
+    def position_mean_calculation(binned_pos_x, binned_pos_y, binned_pos_z):
+        import numpy as np
+
+        med_binned_pos_x = np.mean(binned_pos_x)        #Calculating the mean of the position data for smoothing
+        med_binned_pos_y = np.mean(binned_pos_y)
+        med_binned_pos_z = np.mean(binned_pos_z)
+
+        return med_binned_pos_x, med_binned_pos_y, med_binned_pos_z
+
+    @staticmethod
+    def time_mean_calculation(index, bin_input, binned_time):
+        if index > bin_input:   #Calculates the mean of the binned time data for velocity calculation
+            mean_bin_time = ((binned_time[int(bin_input - 1)]- binned_time[0]) / int(bin_input - 1))    #Numpy does not function for mean time
+        else:                   #Sets variable to zero until enough data is in for valid calculations
+            mean_bin_time = 0
+
+        return mean_bin_time
+
+    @staticmethod
+    def update_previous_bins(binned_pos_x, binned_pos_y, binned_pos_z):
+        import numpy as np
+
+        prev_bin_pos_x = binned_pos_x           #Updates the previous x position bin
+        med_prev_bin_pos_x = np.mean(prev_bin_pos_x)    #Calculates the mean of the previous x position data
+
+        prev_bin_pos_y = binned_pos_y           #Updates the previous x position bin
+        med_prev_bin_pos_y = np.mean(prev_bin_pos_y)    #Calculates the mean of the previous x position data
+
+        prev_bin_pos_z = binned_pos_z           #Updates the previous x position bin
+        med_prev_bin_pos_z = np.mean(prev_bin_pos_z)    #Calculates the mean of the previous x position data
+
+        return med_prev_bin_pos_x, med_prev_bin_pos_y, med_prev_bin_pos_z
