b419fb7df42684834b51186e67a9a6af6b820179
Glen-Skahill <gskahill@gmail.com>
2017-08-08 13:11:44 -0700

    Total Distance and Velocity Bins
    3D Positioning now has a working and mostly reliable total distance function along with a velocity bin function to track the distance and time at certain speeds of the device.

--- house_code/main_programs/PSUPozyx/3D_positioning.py
+++ house_code/main_programs/PSUPozyx/3D_positioning.py
@@ -4,22 +4,40 @@ The Pozyx ready to localize tutorial (c) Pozyx Labs
 Please read the tutorial that accompanies this sketch:
 https://www.pozyx.io/Documentation/Tutorials/ready_to_localize/Python
 
-This tutorial requires at least the contents of the Pozyx Ready to Localize kit. It demonstrates the
-positioning capabilities of the Pozyx device both locally and remotely. Follow the steps to correctly
-set up your environment in the link, change the parameters and upload this sketch. Watch the
-coordinates change as you move your device around!
+This tutorial requires at least the contents of the Pozyx Ready to Localize kit. It demonstrates the positioning capabilities
+of the Pozyx device both locally and remotely. Follow the steps to correctly set up your environment in the link, change the
+parameters and upload this sketch. Watch the coordinates change as you move your device around!
 """
+
+
+"""
+PSU Rm14 Notes:
+This is a version of ready_to_localize designed to both print the data it collects in the console and log it externally to a file.
+This program will create a new program with the title 'localize log YYYY-MM-DD HH-MM-SS' each time it is run. Please note (at least
+on Windows) the file created will be made in the working directory of your command prompt, not necessarily this program's location.
+
+Open the file corresponding to when you ran the program to see the data that was collected. To abort, close the terminal or stop the
+loop (ctrl+c on Windows).
+
+The anchor setup is for Room 14 of the PSU SB1.
+"""
+
 from time import sleep
+from datetime import datetime #for creating the file with date and time in title
+
 from pypozyx import *
-# from pythonosc.osc_message_builder import OscMessageBuilder
+from pythonosc.osc_message_builder import OscMessageBuilder
 from pythonosc.udp_client import SimpleUDPClient
 import time as t
+from modules.user_input_config_functions import UserInputConfigFunctions as UserInput
 from modules.file_writing import SensorAndPositionFileWriting as FileWriting
 from modules.console_logging_functions import ConsoleLoggingFunctions as ConsoleLogging
 from modules.configuration import Configuration as Configuration
 from modules.data_averaging import BinData as BinData
+import numpy as np
 from modules.data_functions import DataFunctions as DataFunctions
 from modules.data_functions import Velocity as Velocity
+from collections import deque
 """
 #RealTimePlotting
 from modules.real_time_plot import RealTimePlot
@@ -27,18 +45,17 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 """
 
-
 class ReadyToLocalize(object):
     """Continuously calls the Pozyx positioning function and prints its position."""
-    def __init__(self, my_pozyx, my_osc_udp_client, my_anchors, my_algorithm=POZYX_POS_ALG_UWB_ONLY,
-                 my_dimension=POZYX_3D, my_height=1000, my_remote_id=None):
-        self.pozyx = my_pozyx
-        self.osc_udp_client = my_osc_udp_client
-        self.anchors = my_anchors
-        self.algorithm = my_algorithm
-        self.dimension = my_dimension
-        self.height = my_height
-        self.remote_id = my_remote_id
+    def __init__(self, pozyx, osc_udp_client, anchors, algorithm=POZYX_POS_ALG_UWB_ONLY, dimension=POZYX_3D, height=1000, remote_id=None):
+        self.pozyx = pozyx
+        self.osc_udp_client = osc_udp_client
+
+        self.anchors = anchors
+        self.algorithm = algorithm
+        self.dimension = dimension
+        self.height = height
+        self.remote_id = remote_id
 
     def setup(self):
         """Sets up the Pozyx for positioning by calibrating its anchor list."""
@@ -53,23 +70,25 @@ class ReadyToLocalize(object):
         print()
         print("START Ranging: ")
         self.pozyx.clearDevices(self.remote_id)
-        self.set_anchors_manual()
-        self.print_publish_configuration_result()
-        # network_id = self.remote_id
+        self.setAnchorsManual()
+        self.printPublishConfigurationResult()
+        network_id = self.remote_id
 
     def loop(self):
         """Performs positioning and displays/exports the results."""
         position = Coordinates()
-        loop_status = self.pozyx.doPositioning(
+        status = self.pozyx.doPositioning(
             position, self.dimension, self.height, self.algorithm, remote_id=self.remote_id)
-        if loop_status == POZYX_SUCCESS:
-            self.print_publish_position(position)
-            return position, loop_status
+        if status == POZYX_SUCCESS:
+            self.printPublishPosition(position)
+            return position, status
         else:
-            self.print_publish_error_code("positioning")
-            return position, loop_status
+            self.printPublishErrorCode("positioning")
+            return position, status
 
-    def print_publish_position(self, position):
+        return "unexpected error"
+
+    def printPublishPosition(self, position):
         """Prints the Pozyx's position and possibly sends it as a OSC packet"""
         network_id = self.remote_id
         if network_id is None:
@@ -78,7 +97,7 @@ class ReadyToLocalize(object):
             self.osc_udp_client.send_message(
                 "/position", [network_id, int(position.x), int(position.y), int(position.z)])
 
-    def print_publish_error_code(self, operation):
+    def printPublishErrorCode(self, operation):
         """Prints the Pozyx's error and possibly sends it as a OSC packet"""
         error_code = SingleRegister()
         network_id = self.remote_id
@@ -88,8 +107,8 @@ class ReadyToLocalize(object):
             if self.osc_udp_client is not None:
                 self.osc_udp_client.send_message("/error", [operation, 0, error_code[0]])
             return
-        error_status = self.pozyx.getErrorCode(error_code, self.remote_id)
-        if error_status == POZYX_SUCCESS:
+        status = self.pozyx.getErrorCode(error_code, self.remote_id)
+        if status == POZYX_SUCCESS:
             print("ERROR %s on ID %s, error code %s" %
                   (operation, "0x%0.4x" % network_id, str(error_code)))
             if self.osc_udp_client is not None:
@@ -103,33 +122,33 @@ class ReadyToLocalize(object):
                 self.osc_udp_client.send_message("/error", [operation, 0, -1])
             # should only happen when not being able to communicate with a remote Pozyx.
 
-    def set_anchors_manual(self):
+    def setAnchorsManual(self):
         """Adds the manually measured anchors to the Pozyx's device list one for one."""
-        manual_status = self.pozyx.clearDevices(self.remote_id)
+        status = self.pozyx.clearDevices(self.remote_id)
         for anchor in self.anchors:
-            manual_status &= self.pozyx.addDevice(anchor, self.remote_id)
+            status &= self.pozyx.addDevice(anchor, self.remote_id)
         if len(anchors) > 4:
-            manual_status &= self.pozyx.setSelectionOfAnchors(POZYX_ANCHOR_SEL_AUTO, len(anchors))
-        return manual_status
+            status &= self.pozyx.setSelectionOfAnchors(POZYX_ANCHOR_SEL_AUTO, len(anchors))
+        return status
 
-    def print_publish_configuration_result(self):
+    def printPublishConfigurationResult(self):
         """Prints and potentially publishes the anchor configuration result in a human-readable way."""
         list_size = SingleRegister()
 
-        result_status = self.pozyx.getDeviceListSize(list_size, self.remote_id)
+        status = self.pozyx.getDeviceListSize(list_size, self.remote_id)
         print("List size: {0}".format(list_size[0]))
         if list_size[0] != len(self.anchors):
-            self.print_publish_error_code("configuration")
+            self.printPublishErrorCode("configuration")
             return
         device_list = DeviceList(list_size=list_size[0])
-        result_status = self.pozyx.getDeviceIds(device_list, self.remote_id)
+        status = self.pozyx.getDeviceIds(device_list, self.remote_id)
         print("Calibration result:")
         print("Anchors found: {0}".format(list_size[0]))
         print("Anchor IDs: ", device_list)
 
         for i in range(list_size[0]):
             anchor_coordinates = Coordinates()
-            result_status = self.pozyx.getDeviceCoordinates(
+            status = self.pozyx.getDeviceCoordinates(
                 device_list[i], anchor_coordinates, self.remote_id)
             print("ANCHOR,0x%0.4x, %s" % (device_list[i], str(anchor_coordinates)))
             if self.osc_udp_client is not None:
@@ -137,21 +156,21 @@ class ReadyToLocalize(object):
                     "/anchor", [device_list[i], int(anchor_coordinates.x), int(anchor_coordinates.y), int(anchor_coordinates.z)])
                 sleep(0.025)
 
-    def print_publish_anchor_configuration(self):
+    def printPublishAnchorConfiguration(self):
         """Prints and potentially publishes the anchor configuration"""
         for anchor in self.anchors:
             print("ANCHOR,0x%0.4x,%s" % (anchor.network_id, str(anchor.coordinates)))
             if self.osc_udp_client is not None:
                 self.osc_udp_client.send_message(
-                    "/anchor", [anchor.network_id, int(anchor_coordinates.x),
-                                int(anchor_coordinates.y), int(anchor_coordinates.z)])
-                    # "/anchor", [anchor.network_id, int(anchor.coordinates.x),
-                    #             int(anchor.coordinates.y), int(anchor.coordinates.z)])
+                    "/anchor", [anchor.network_id, int(anchor_coordinates.x), int(anchor_coordinates.y), int(anchor_coordinates.z)])
                 sleep(0.025)
 
 
-if __name__ == "__main__":
+
+
+if  __name__ == "__main__":
     serial_port = Configuration.get_correct_serial_port()
+
 
     remote = True                  # whether to use a remote device
     if not remote:
@@ -174,7 +193,7 @@ if __name__ == "__main__":
     if use_processing:
         osc_udp_client = SimpleUDPClient(ip, network_port)
 
-    # algorithm = POZYX_POS_ALG_UWB_ONLY  # positioning algorithm to use
+    #algorithm = POZYX_POS_ALG_UWB_ONLY  # positioning algorithm to use
     algorithm = POZYX_POS_ALG_TRACKING  # tracking positioning algorithm
     dimension = POZYX_3D               # positioning dimension
     height = 1000                      # height of device, required in 2.5D positioning
@@ -193,6 +212,7 @@ if __name__ == "__main__":
         else:
             FileWriting.write_position_header_to_file(logfile)
 
+
     """
     #RealTimePlotting
     fig,axes = plt.subplots()
@@ -201,87 +221,83 @@ if __name__ == "__main__":
     plt.ylabel("X Velocity")
     #To add more subplots, copy this code and change the object name
     """
-    bin_pos_x, bin_pos_y, bin_pos_z, bin_time = None, None, None, None
-    med_prev_bin_pos_x, med_prev_bin_pos_y, med_prev_bin_pos_z = None, None, None
-
     if use_velocity:
         bin_input = DataFunctions.bin_input()
 
-        bin_pos_x = BinData(bin_size=bin_input)   # Creating position deque objects to calculate velocity
-        prev_bin_pos_x = 0                          # Initializing the previous points
+        bin_pos_x = BinData(bin_size = bin_input)   #Creating position deque objects to calculate velocity
+        prev_bin_pos_x = 0                          #Initializing the previous points
 
-        bin_pos_y = BinData(bin_size=bin_input)
+        bin_pos_y = BinData(bin_size = bin_input)
         prev_bin_pos_y = 0
 
-        bin_pos_z = BinData(bin_size=bin_input)
+        bin_pos_z = BinData(bin_size = bin_input)
         prev_bin_pos_z = 0
 
-        bin_time = BinData(bin_size=bin_input)
+        bin_time = BinData(bin_size = bin_input)
 
-        med_prev_bin_pos_x = 0      # Initializing median calculation variables
-        med_prev_bin_pos_y = 0
-        med_prev_bin_pos_z = 0
+        mean_prev_bin_pos_x = 0      #Initializing mean calculation variables
+        mean_prev_bin_pos_y = 0
+        mean_prev_bin_pos_z = 0
+
+        total_distance = 0             #Initializing total distance
+        time_between_2500_and_4500 = 0              #Initializing different bins for velocity intervals
+        time_between_4500_and_6500 = 0
+        time_between_6500_and_8500 = 0
+        time_above_8500 = 0
 
     start = t.time()
     try:
         while True:
-            elapsed = (t.time() - start)
-            oldTime = newTime
-            newTime = elapsed
-            timeDifference = newTime - oldTime
+            elapsed=(t.time()-start)                              # elapsed time since the program started
+            oldTime = newTime                                     # oldTime is the time of previous cycle. It is set to newTime here since newTime has not been updated and still is the old cycle
+            newTime = elapsed                                     # newTime is the time of the current cycle.
+            timeDifference = newTime - oldTime                    # timeDifference is the differece in time between each subsequent cycle
 
-            # the loop method of r prints data to the console and returns what is printed
-            one_cycle_position, status = r.loop()
-            # Status is used for error handling
+            one_cycle_position, status = r.loop()    # the loop method of r prints data to the console and returns what is printed
+            #Status is used for error handling
 
             if use_velocity:
-                # Updates and returns the new bins
-                binned_pos_x, binned_pos_y, binned_pos_z, binned_time = Velocity.update_bins(
-                    bin_pos_x, bin_pos_y, bin_pos_z, bin_time, timeDifference, one_cycle_position)
-                # Returns the means of the position bins
-                # med_binned_pos_x, med_binned_pos_y, med_binned_pos_z =
-                #   Velocity.position_mean_calculation(binned_pos_x, binned_pos_y, binned_pos_z)
-                # returns the mean of the time bin
+                #Updates and returns the new bins
+                binned_pos_x, binned_pos_y, binned_pos_z, binned_time = Velocity.update_bins(bin_pos_x, bin_pos_y, bin_pos_z,
+                    bin_time, timeDifference, one_cycle_position)
 
-                # Can equal either simple or linreg
+                #Can equal either simple or linreg
                 velocity_method = 'simple'
-                # velocity_method = 'linreg'
+                #velocity_method = 'linreg'
 
-                # Calculates the directional velocities, set the method using method argument
-                velocity_x = Velocity.find_velocity(index, bin_input, binned_pos_x, med_prev_bin_pos_x,
-                                                    binned_time, method=velocity_method)    # Calculates x velocity
-                velocity_y = Velocity.find_velocity(index, bin_input, binned_pos_y, med_prev_bin_pos_y,
-                                                    binned_time, method=velocity_method)    # Calculates y velocity
-                velocity_z = Velocity.find_velocity(index, bin_input, binned_pos_z, med_prev_bin_pos_z,
-                                                    binned_time, method=velocity_method)    # Calculates z velocity
-                # gets the medians of the previous position bins for calculations next loop
+                #Calculates the directional velocities, set the method using method argument
+                velocity_x = Velocity.find_velocity(index, bin_input, binned_pos_x, mean_prev_bin_pos_x, binned_time, method = velocity_method)    #Calculates x velocity
+                velocity_y = Velocity.find_velocity(index, bin_input, binned_pos_y, mean_prev_bin_pos_y, binned_time, method = velocity_method)    #Calculates y velocity
+                velocity_z = Velocity.find_velocity(index, bin_input, binned_pos_z, mean_prev_bin_pos_z, binned_time, method = velocity_method)    #Calculates z velocity
 
-                # Gets the means of the previous data for calculations
-                med_prev_bin_pos_x, med_prev_bin_pos_y, med_prev_bin_pos_z = Velocity.update_previous_bins(
-                    binned_pos_x, binned_pos_y, binned_pos_z)
+                total_distance, total_velocity = DataFunctions.find_total_distance(binned_pos_x, binned_pos_y, binned_pos_z,
+                    mean_prev_bin_pos_x, mean_prev_bin_pos_y, mean_prev_bin_pos_z, velocity_x, velocity_y, velocity_z, total_distance)
 
-            # Logs the data to console
+                time_between_2500_and_4500, time_between_4500_and_6500, time_between_6500_and_8500, time_above_8500 = DataFunctions.velocity_bins(total_velocity,
+                    time_between_2500_and_4500, time_between_4500_and_6500, time_between_6500_and_8500, time_above_8500, timeDifference)
+
+
+                #Gets the means of the previous data for calculations
+                mean_prev_bin_pos_x, mean_prev_bin_pos_y, mean_prev_bin_pos_z = Velocity.update_previous_bins(binned_pos_x, binned_pos_y, binned_pos_z)
+
+
+            #Logs the data to console
             if use_velocity:
-                ConsoleLogging.log_position_and_velocity_to_console(
-                    index, elapsed, one_cycle_position, velocity_x, velocity_y, velocity_z)
+                ConsoleLogging.log_position_and_velocity_to_console(index, elapsed, one_cycle_position, velocity_x, velocity_y, velocity_z)
             else:
                 ConsoleLogging.log_position_to_console(index, elapsed, one_cycle_position)
 
+
             if to_use_file:             # writes the data returned from the loop method to the file
                 if use_velocity:
-                    if index > bin_input:   # Accounts for the time it takes to get accurate velocity calculations
-                        FileWriting.write_position_and_velocity_data_to_file(
-                            index, elapsed, timeDifference, logfile, one_cycle_position,
-                            velocity_x, velocity_y, velocity_z)
-                    else:                   # Returns 0 for velocity until it provides complete calculations
-                        FileWriting.write_position_and_velocity_data_to_file(
-                            index, elapsed, timeDifference, logfile, one_cycle_position,
-                            0, 0, 0)
+                    if index > bin_input:   #Accounts for the time it takes to get accurate velocity calculations
+                        FileWriting.write_position_and_velocity_data_to_file(index, elapsed, timeDifference, logfile, one_cycle_position, velocity_x, velocity_y, velocity_z)
+                    else:                   #Returns 0 for velocity until it provides complete calculations
+                        FileWriting.write_position_and_velocity_data_to_file(index, elapsed, timeDifference, logfile, one_cycle_position, 0, 0, 0)
                 else:
-                    FileWriting.write_position_data_to_file(
-                        index, elapsed, timeDifference, logfile, one_cycle_position)
+                    FileWriting.write_position_data_to_file(index, elapsed, timeDifference, logfile, one_cycle_position)
 
-            index = index + 1
+            index = index + 1                                     # increment data index
 
             """
             #RealTimePlotting which significantly decreases Hz
