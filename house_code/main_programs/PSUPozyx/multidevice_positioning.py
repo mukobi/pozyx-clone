74758fc235937425590870271b1541fe1627e20a
Gabe Mukobi <gabrielmukobi@gmail.com>
2017-08-08 12:15:27 -0700

    Make more stuff prettier

--- house_code/main_programs/PSUPozyx/multidevice_positioning.py
+++ house_code/main_programs/PSUPozyx/multidevice_positioning.py
@@ -1,20 +1,20 @@
-
 #!/usr/bin/env python
 """
 The Pozyx ready to localize tutorial (c) Pozyx Labs
 Please read the tutorial that accompanies this sketch:
 https://www.pozyx.io/Documentation/Tutorials/ready_to_localize/Python
 
-This tutorial requires at least the contents of the Pozyx Ready to Localize kit. It demonstrates the positioning capabilities
-of the Pozyx device both locally and remotely. Follow the steps to correctly set up your environment in the link, change the
-parameters and upload this sketch. Watch the coordinates change as you move your device around!
+This tutorial requires at least the contents of the Pozyx Ready to Localize kit. It demonstrates the
+positioning capabilities of the Pozyx device both locally and remotely. Follow the steps to correctly
+set up your environment in the link, change the parameters and upload this sketch. Watch the
+coordinates change as you move your device around!
 
 """
-from time import sleep, time
+from time import sleep
 
 from pypozyx import *
-from pypozyx.definitions.registers import POZYX_EUL_HEADING
-from pythonosc.osc_message_builder import OscMessageBuilder
+# from pypozyx.definitions.registers import POZYX_EUL_HEADING
+# from pythonosc.osc_message_builder import OscMessageBuilder
 from pythonosc.udp_client import SimpleUDPClient
 from modules.console_logging_functions import ConsoleLoggingFunctions as ConsoleLogging
 from modules.configuration import Configuration as Configuration
@@ -23,22 +23,26 @@ from modules.configuration import Configuration as Configuration
 class MultitagPositioning(object):
     """Continuously performs multitag positioning"""
 
-    def __init__(self, pozyx, osc_udp_client, tags, anchors, algorithm=POZYX_POS_ALG_UWB_ONLY, dimension=POZYX_3D, height=1000, remote_id=None):
-        self.pozyx = pozyx
-        self.osc_udp_client = osc_udp_client
+    def __init__(self, my_pozyx, my_osc_udp_client, my_tags, my_anchors, my_algorithm=POZYX_POS_ALG_UWB_ONLY,
+                 my_dimension=POZYX_3D, my_height=1000, my_remote_id=None):
+        self.pozyx = my_pozyx
+        self.osc_udp_client = my_osc_udp_client
 
-        self.tags = tags
-        self.anchors = anchors
-        self.algorithm = algorithm
-        self.dimension = dimension
-        self.height = height
-        self.remote_id = remote_id
+        self.tags = my_tags
+        self.anchors = my_anchors
+        self.algorithm = my_algorithm
+        self.dimension = my_dimension
+        self.height = my_height
+        self.remote_id = my_remote_id
 
     def setup(self):
         """Sets up the Pozyx for positioning by calibrating its anchor list."""
-        print("------------POZYX MULTITAG POSITIONING V1.1 - -----------\nNOTES: \n- Parameters required:\n\t- Anchors for calibration\n\t- Tags to work with\n\n- System will manually calibration\n\n- System will auto start positioning\n- -----------POZYX MULTITAG POSITIONING V1.1 ------------\nSTART Positioning: ")
-        self.setAnchorsManual()
-        self.printPublishAnchorConfiguration()
+        print("------------POZYX MULTITAG POSITIONING V1.1 - -----------\nNOTES: \n- "
+              "Parameters required:\n\t- Anchors for calibration\n\t- Tags to work with\n\n- "
+              "System will manually calibration\n\n- System will auto start positioning\n- "
+              "-----------POZYX MULTITAG POSITIONING V1.1 ------------\nSTART Positioning: ")
+        self.set_anchors_manual()
+        self.print_publish_anchor_configuration()
 
     def loop(self):
         """Performs positioning and prints the results."""
@@ -49,28 +53,29 @@ class MultitagPositioning(object):
                 position, self.dimension, self.height, self.algorithm, remote_id=tag)
             coordinate_array.append(tag)
             if status == POZYX_SUCCESS:
-                self.printPublishPosition(position, tag)
+                self.print_publish_position(position, tag)
                 coordinate_array.append(position.x)
                 coordinate_array.append(position.y)
                 coordinate_array.append(position.z)
             else:
-                self.printPublishErrorCode("positioning", tag)
+                self.print_publish_error_code("positioning", tag)
                 coordinate_array.append(position)
         # [0x6001, 244, 255, 65, 0x6002, 7677, 7656, 543, 0x6003, ... ]
         return coordinate_array
 
-    def printPublishPosition(self, position, network_id):
+    def print_publish_position(self, position, network_id):
         """Prints the Pozyx's position and possibly sends it as a OSC packet"""
         if network_id is None:
             network_id = 0
-        s = "POS ID: {}, x(mm): {}, y(mm): {}, z(mm): {}".format("0x%0.4x" % network_id, position.x, position.y, position.z)
-        # uncommet to have default console printing
+        # uncomment to have default console printing
+        # s = "POS ID: {}, x(mm): {}, y(mm): {}, z(mm): {}".format(
+        #     "0x%0.4x" % network_id, position.x, position.y, position.z)
         # print(s)
         if self.osc_udp_client is not None:
             self.osc_udp_client.send_message(
                 "/position", [network_id, position.x, position.y, position.z])
 
-    def setAnchorsManual(self):
+    def set_anchors_manual(self):
         """Adds the manually measured anchors to the Pozyx's device list one for one."""
         for tag in self.tags:
             status = self.pozyx.clearDevices(tag)
@@ -81,18 +86,18 @@ class MultitagPositioning(object):
             # enable these if you want to save the configuration to the devices.
             # self.pozyx.saveAnchorIds(tag)
             # self.pozyx.saveRegisters([POZYX_ANCHOR_SEL_AUTO], tag)
-            self.printPublishConfigurationResult(status, tag)
+            self.print_publish_configuration_result(status, tag)
 
-    def printPublishConfigurationResult(self, status, tag_id):
+    def print_publish_configuration_result(self, status, tag_id):
         """Prints the configuration explicit result, prints and publishes error if one occurs"""
         if tag_id is None:
             tag_id = 0
         if status == POZYX_SUCCESS:
             print("Configuration of tag %s: success" % tag_id)
         else:
-            self.printPublishErrorCode("configuration", tag_id)
+            self.print_publish_error_code("configuration", tag_id)
 
-    def printPublishErrorCode(self, operation, network_id):
+    def print_publish_error_code(self, operation, network_id):
         """Prints the Pozyx's error and possibly sends it as a OSC packet"""
         error_code = SingleRegister()
         status = self.pozyx.getErrorCode(error_code, self.remote_id)
@@ -111,7 +116,7 @@ class MultitagPositioning(object):
             if self.osc_udp_client is not None:
                 self.osc_udp_client.send_message("/error_%s" % operation, [0, error_code[0]])
 
-    def printPublishAnchorConfiguration(self):
+    def print_publish_anchor_configuration(self):
         for anchor in self.anchors:
             print("ANCHOR,0x%0.4x,%s" % (anchor.network_id, str(anchor.pos)))
             if self.osc_udp_client is not None:
