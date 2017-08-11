6ed867e81559e22a4fbf488b96b8e651caad11f5
Gabe Mukobi <gabrielmukobi@gmail.com>
2017-08-11 15:26:00 -0700

    Maybe fix FXML Gui error

--- house_code/main_programs/PSUPozyx/src/psupozyx/Main.java
+++ house_code/main_programs/PSUPozyx/src/psupozyx/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("layout_main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/psupozyx/layout_main.fxml"));
         primaryStage.setTitle("PSU Pozyx");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
