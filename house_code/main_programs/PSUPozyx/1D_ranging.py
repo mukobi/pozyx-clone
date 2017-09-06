655ceaf4751995af15703e5707710ca7cda272a8
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-09-05 19:43:34 -0700

    Collapse 1D files down to 1 smart file for ranging or ranging + motion and adapt GUI to run it

--- house_code/main_programs/PSUPozyx/1D_ranging.py
+++ house_code/main_programs/PSUPozyx/1D_ranging.py
@@ -8,13 +8,16 @@ This demo measures the range between the two devices. The closer the devices are
 light up on both devices.
 """
 
+
+from time import time
 from pypozyx import *
+from pypozyx.definitions.bitmasks import POZYX_INT_MASK_IMU
+from pythonosc.osc_message_builder import OscMessageBuilder
 from pythonosc.udp_client import SimpleUDPClient
 import time as t
 from modules.file_writing import SensorAndPositionFileWriting as FileWriting
 from modules.console_logging_functions import ConsoleLoggingFunctions as ConsoleLogging
 from modules.configuration import Configuration as Configuration
-from modules.data_functions import DataFunctions as DataFunctions
 from modules.data_functions import Velocity as Velocity
 from collections import deque
 import copy as copy
@@ -31,14 +34,11 @@ class ReadyToRange(object):
         self.remote_id = i_remote_id
         self.protocol = i_protocol
         self.osc_udp_client = i_osc_udp_client
+        self.current_time = None
+        self.msg_builder = None
 
     def setup(self):
         """Sets up both the ranging and destination Pozyx's LED configuration"""
-        print("------------POZYX RANGING V1.0 - -----------")
-        print("- Approach target device to see range and")
-        print("led control")
-        print("START Ranging: ")
-
         # make sure the local/remote pozyx system has no control over the LEDs.
         led_config = 0x0
         self.pozyx.setLedConfig(led_config, self.remote_id)
@@ -46,19 +46,32 @@ class ReadyToRange(object):
         self.pozyx.setLedConfig(led_config, self.destination_id)
         # set the ranging protocol
         self.pozyx.setRangingProtocol(self.protocol, self.remote_id)
+        self.current_time = time()
 
-    def loop(self):
+    def loop(self, to_get_sensor_data):
         """Performs ranging and sets the LEDs accordingly"""
         device_range = DeviceRange()
-        ranging_status = self.pozyx.doRanging(self.destination_id, device_range, self.remote_id)
 
-        if ranging_status == POZYX_SUCCESS:
+        calibration_status = SingleRegister()
+        loop_status = self.pozyx.doRanging(self.destination_id, device_range, self.remote_id)
+
+        # get 1D position in this section
+        if loop_status == POZYX_SUCCESS:
             self.print_publish_position(device_range)
-            return device_range, ranging_status
         else:
-            device_range.timestamp, device_range.distance, device_range.RSS = \
+            device_range.timestamp, device_range.distance, device_range.rss =\
                 "ranging-error", "ranging-error", "ranging-error"
-            return device_range, ranging_status
+
+        # get motion data in this section
+        sensor_data = SensorData()
+        if to_get_sensor_data:
+            sensor_data.data_format = 'IhhhhhhhhhhhhhhhhhhhhhhB'
+            if self.remote_id is not None or self.pozyx.checkForFlag(POZYX_INT_MASK_IMU, 0.01) == POZYX_SUCCESS:
+                loop_status = self.pozyx.getAllSensorData(sensor_data, self.remote_id)
+                loop_status &= self.pozyx.getCalibrationStatus(calibration_status, self.remote_id)
+                if loop_status == POZYX_SUCCESS:
+                    self.publish_sensor_data(sensor_data, calibration_status)
+        return device_range, sensor_data, loop_status
 
     def print_publish_position(self, device_range):
         """Prints the Pozyx's position and possibly sends it as a OSC packet"""
@@ -107,6 +120,38 @@ class ReadyToRange(object):
             led_status &= self.pozyx.setLed(1, (distance < 4 * range_step_mm), single_id)
         return led_status
 
+    def publish_sensor_data(self, sensor_data, calibration_status):
+        """Makes the OSC sensor data package and publishes it"""
+        self.msg_builder = OscMessageBuilder("/sensordata")
+        self.msg_builder.add_arg(int(1000 * (time() - self.current_time)))
+        # current_time = time()
+        self.add_sensor_data(sensor_data)
+        self.add_calibration_status(calibration_status)
+        self.osc_udp_client.send(self.msg_builder.build())
+
+    def add_sensor_data(self, sensor_data):
+        """Adds the sensor data to the OSC message"""
+        self.msg_builder.add_arg(sensor_data.pressure)
+        self.add_components_osc(sensor_data.acceleration)
+        self.add_components_osc(sensor_data.magnetic)
+        self.add_components_osc(sensor_data.angular_vel)
+        self.add_components_osc(sensor_data.euler_angles)
+        self.add_components_osc(sensor_data.quaternion)
+        self.add_components_osc(sensor_data.linear_acceleration)
+        self.add_components_osc(sensor_data.gravity_vector)
+
+    def add_components_osc(self, component):
+        """Adds a sensor data component to the OSC message"""
+        for data in component.data:
+            self.msg_builder.add_arg(float(data))
+
+    def add_calibration_status(self, calibration_status):
+        """Adds the calibration status data to the OSC message"""
+        self.msg_builder.add_arg(calibration_status[0] & 0x03)
+        self.msg_builder.add_arg((calibration_status[0] & 0x0C) >> 2)
+        self.msg_builder.add_arg((calibration_status[0] & 0x30) >> 4)
+        self.msg_builder.add_arg((calibration_status[0] & 0xC0) >> 6)
+
 if __name__ == "__main__":
     serial_port = Configuration.get_correct_serial_port()
 
@@ -115,6 +160,8 @@ if __name__ == "__main__":
     # import properties from saved properties file
     (remote, remote_id, tags, anchors, attributes_to_log, to_use_file,
         filename, use_processing) = Configuration.get_properties()
+
+    to_get_sensor_data = not attributes_to_log == []
 
     ip, network_port, osc_udp_client = "127.0.0.1", 8888, None
     osc_udp_client = SimpleUDPClient(ip, network_port)
@@ -140,7 +187,8 @@ if __name__ == "__main__":
 
     if use_velocity:
         # *** this function should be in the user_input_config_functions module, not data functions ***
-        bin_input = DataFunctions.bin_input()       # Determines how many points the user wants to bin
+        # bin_input = DataFunctions.bin_input()       # Determines how many points the user wants to bin
+        bin_input = 10  # hard coded for easy use
 
         bin_pos = deque(maxlen=bin_input)
         prev_bin_pos = deque(maxlen=bin_input)
@@ -154,19 +202,22 @@ if __name__ == "__main__":
         start = t.time()
         newTime = start
         while True:
-            elapsed = (t.time()-start)
+            elapsed = t.time() - start
             oldTime = newTime
             newTime = elapsed
             timeDifference = newTime - oldTime
 
-            one_cycle_position, status = r.loop()
+            # Status is used for error handling
+            one_cycle_position, one_cycle_motion_data, status = r.loop(to_get_sensor_data)
 
-            if use_velocity and status == POZYX_SUCCESS and one_cycle_position != 0:
+            if use_velocity and status == POZYX_SUCCESS and one_cycle_position != 0 \
+                    and type(one_cycle_position.distance) != str:
                 # Can equal either simple or linreg
                 velocity_method = 'simple'
 
                 bin_pos.append(one_cycle_position.distance)
                 bin_time.append(newTime)
+
                 # Calculates the directional velocities, set the method using method argument
                 velocity = Velocity.find_velocity1D(
                     bin_input, bin_pos, prev_bin_pos, bin_time, prev_bin_time, velocity_method)
@@ -175,10 +226,16 @@ if __name__ == "__main__":
                 velocity = 'calc-error'
 
             # Logs the data to console
+            formatted_motion_data_dictionary = ConsoleLogging.format_sensor_data(
+                one_cycle_motion_data, attributes_to_log)
             if use_velocity:
-                ConsoleLogging.log_position_and_velocity_to_console_1d(index, elapsed, one_cycle_position, velocity)
+                ConsoleLogging.log_range_motion_and_velocity(
+                    index, elapsed, one_cycle_position,
+                    formatted_motion_data_dictionary, velocity)
             else:
-                ConsoleLogging.log_position_to_console_1d(index, elapsed, one_cycle_position)
+                ConsoleLogging.log_range_and_motion(
+                    index, elapsed, one_cycle_position,
+                    formatted_motion_data_dictionary)
 
             # writes the data to a file
             if to_use_file:
