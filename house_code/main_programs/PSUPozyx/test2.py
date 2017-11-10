50abb1d8366929f42d99984c67e9bf6eb25cb071
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-11-09 23:06:07 -0800

    mpl animation working for ranging

---
+++ house_code/main_programs/PSUPozyx/test2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
