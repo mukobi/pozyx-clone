7aa6e6fabca120a3f280f83c5440e14cd72ed19c
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-12-04 20:11:11 -0800

    Add original motion data file to commit

--- house_code/main_programs/PSUPozyx/motion_data.py
+++ house_code/main_programs/PSUPozyx/motion_data.py
@@ -20,92 +20,109 @@ This demo reads the following sensor data:
 The data can be viewed in the Processing sketch orientation_3D.pde
 """
 import sys
-from time import time
+import time
 from pypozyx import *
 from pypozyx.definitions.bitmasks import POZYX_INT_MASK_IMU
 from pythonosc.udp_client import SimpleUDPClient
-from modules.file_writing import SensorDataFileWriting as FileWriting
-from modules.console_logging_functions import ConsoleLoggingFunctions as ConsoleLogging
+from modules.file_writing import SensorDataFileWriting as FileIO
+from modules.console_logging_functions import CondensedConsoleLogging as Console
 from modules.configuration import Configuration as Configuration
 from modules.pozyx_osc import PozyxOSC
 sys.path.append(sys.path[0] + "/..")
 from constants import definitions
 
 
+class MotionDataOutputContainer:
+    """Holds the range data, motion data, and more for a single device"""
+    def __init__(self, tags, sensor_data, loop_status):
+        self.tags = tags
+        self.sensor_data = sensor_data
+        self.loop_status = loop_status
+
+
 class Orientation3D(object):
     """Reads out all sensor data from either a local or remote Pozyx"""
-    def __init__(self, in_pozyx, in_remote_id=None):
+    def __init__(self, in_pozyx, in_tags, in_remote_id=None):
         self.pozyx = in_pozyx
+        self.tags = in_tags
         self.remote_id = in_remote_id
 
-    def loop(self):
+    def loop(self, loop_data_array):
         """Gets new IMU sensor data"""
-        sensor_data = SensorData()
-        # override data format in pozyx library that results in bad pressure
-        sensor_data.data_format = 'IhhhhhhhhhhhhhhhhhhhhhhB'
-        calibration_status = SingleRegister()
-        if self.remote_id is not None or self.pozyx.checkForFlag(POZYX_INT_MASK_IMU, 0.01) == POZYX_SUCCESS:
-            status = self.pozyx.getAllSensorData(sensor_data, self.remote_id)
-            status &= self.pozyx.getCalibrationStatus(calibration_status, self.remote_id)
-            if status == POZYX_SUCCESS:
-                return sensor_data
+        for idx, tag in enumerate(self.tags):
+            loop_status = 0
+            sensor_data = SensorData()
+            # override data format in pozyx library that results in bad pressure
+            sensor_data.data_format = 'IhhhhhhhhhhhhhhhhhhhhhhB'
+            calibration_status = SingleRegister()
+            if self.remote_id is not None or self.pozyx.checkForFlag(POZYX_INT_MASK_IMU, 0.01) == POZYX_SUCCESS:
+                loop_status = self.pozyx.getAllSensorData(sensor_data, self.remote_id)
+                loop_status &= self.pozyx.getCalibrationStatus(calibration_status, self.remote_id)
 
-        return "Error, no data to print for this line"
+            single = loop_data_array[idx]
+            single.tag = tag
+            single.sensor_data = sensor_data
+            single.loop_status = loop_status
 
 
 if __name__ == '__main__':
     serial_port = Configuration.get_correct_serial_port()
 
-    index = 0
-    previous_cycle_time = 0
-    current_cycle_time = 0
-
     # import properties from saved properties file
     (remote, remote_id, tags, anchors, attributes_to_log, to_use_file,
         filename, use_processing) = Configuration.get_properties()
 
-    use_processing = True
-
     if not remote:
         remote_id = None
 
-    use_processing = True
-
-    ip = "127.0.0.1"
-    network_port = 8888
-
     pozyx = PozyxSerial(serial_port)
-    osc_udp_client = SimpleUDPClient(ip, network_port)
-    o = Orientation3D(pozyx, remote_id)
+    o = Orientation3D(pozyx, tags, remote_id)
+
+    loop_data_array = []
+    for tag in tags:
+        loop_data_array.append(MotionDataOutputContainer(None, None, None))
+    if not tags: # allocate 1 data slot for local device
+        loop_data_array.append(MotionDataOutputContainer(None, None, None))
 
     logfile = None
     if to_use_file:
         logfile = open(filename, 'a')
-        FileWriting.write_sensor_data_header_to_file(logfile)
+        # TODO make this write a standardized header for all tags
+        FileIO.write_sensor_data_header_to_file(logfile)
 
-    start = ConsoleLogging.get_time()
     try:
+        ip, network_port = "127.0.0.1", 8888
+        osc_udp_client = SimpleUDPClient(ip, network_port)
+        pozyxOSC = PozyxOSC(osc_udp_client)
+
+        index = 0
+        start = time.time()
+        new_time = 0.0
         while True:
-            # updates elapsed time and time difference
-            elapsed = ConsoleLogging.get_elapsed_time(ConsoleLogging, start)
-            previous_cycle_time = current_cycle_time
-            current_cycle_time = elapsed
-            time_difference = current_cycle_time - previous_cycle_time
+            elapsed = time.time() - start
+            old_time = new_time
+            new_time = elapsed
+            time_difference = new_time - old_time
 
-            one_cycle_sensor_data = o.loop()
+            o.loop(loop_data_array)
 
-            formatted_data_dictionary = ConsoleLogging.format_sensor_data(
-                one_cycle_sensor_data, attributes_to_log)
-            ConsoleLogging.log_sensor_data_to_console(index, elapsed, formatted_data_dictionary)
+            # TODO implement this in condensed console logging
+            # Console.print_motion_data_output(
+            #     index, elapsed, loop_data_array, attributes_to_log)
+
             if to_use_file:
-                FileWriting.write_line_of_sensor_data_to_file(
-                    index, elapsed, time_difference,
-                    logfile, one_cycle_sensor_data)
+                # TODO make this method write data for all tags
+                FileIO.write_line_of_sensor_data_to_file(index, elapsed, time_difference, logfile, loop_data_array[0].sensor_data)
 
-            index += 1
+            if loop_data_array[0].loop_status == POZYX_SUCCESS:
+                data_type = [definitions.DATA_TYPE_MOTION_DATA]
+                pozyxOSC.send_message(elapsed, tags, loop_data_array, data_type)
+
+            index = index + 1
 
     except KeyboardInterrupt:
         pass
 
-    if to_use_file:
-        logfile.close()
+    finally:
+        if to_use_file:
+            logfile.close()
