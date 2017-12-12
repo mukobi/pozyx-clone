2b77830c52c7e87002ba95635cb278d3cde0de86
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-12-11 21:41:55 -0800

    Revert "Revert "Try implementing message sending with UDP to allow multiple receives from different graphs"" This reverts commit 9e0503b669c34dd8f5231b8c54fa0db66d56ed35.

--- house_code/main_programs/PSUPozyx/RealtimeGraphing/2D_realtime_graphing.py
+++ house_code/main_programs/PSUPozyx/RealtimeGraphing/2D_realtime_graphing.py
@@ -7,6 +7,7 @@ import sys
 import random
 sys.path.append(sys.path[0] + "/..")
 from constants import definitions
+from modules import udp
 
 # global config variables
 data_address = "/pozyx"
@@ -22,6 +23,7 @@ class OSCDataHandling:
         self.x_data = []
         self.y_data = []
         self.maxLen = max_data_length
+        self.consumer = udp.Consumer()
 
     def clear_data(self):
         self.x_data = []
@@ -39,18 +41,18 @@ class OSCDataHandling:
     def change_max_data_len(self, len_in):
         self.maxLen = len_in
 
-    def extract_range_data(self, *args):
-        message = args[0]
+    def extract_data(self, new_data):
+        message = new_data[1]
         # print(message)
         # extract data from osc message
         tag_idx = message.index(self.tag)
 
         x_index = definitions.OSC_INDEX_DICT[self.x_axis] + tag_idx
         if self.x_axis == "time":
-            x_index = 1
+            x_index = 0
         y_index = definitions.OSC_INDEX_DICT[self.y_axis] + tag_idx
         if self.y_axis == "time":
-            y_index = 1
+            y_index = 0
 
         x = message[x_index]
         y = message[y_index]
@@ -66,17 +68,16 @@ class OSCDataHandling:
         if number_y_over > 0:
             self.y_data = self.y_data[number_y_over:]
 
-    def deal_with_data(self, *args):
-        x, y = self.extract_range_data(args)
+    def deal_with_data(self, new_data):
+        x, y = self.extract_data(new_data)
         self.add(x, y)
 
     def start_running(self, *args):
-        my_dispatcher = dispatcher.Dispatcher()
-        # tells the dispatcher to use function to handle range data
-        my_dispatcher.map(data_address, self.deal_with_data)
-        # create server
-        server = osc_server.ThreadingOSCUDPServer((ip, network_code), my_dispatcher)
-        server.serve_forever()
+        while True:
+            new_data = self.consumer.receive()
+            if new_data is None:
+                continue
+            self.deal_with_data(new_data)
 
     def get_data(self):
         return self.x_data, self.y_data
@@ -157,6 +158,7 @@ if __name__ == "__main__":
 
     w.show()
     curve = pw.plot(pen=pen)
+    curve = pw.plot()
 
     def update():
         x, y = osc_handler.get_data()
@@ -184,7 +186,7 @@ if __name__ == "__main__":
 
     timer = QtCore.QTimer()
     timer.timeout.connect(update)
-    timer.start(16)
+    timer.start(50)
 
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         app.exec_()
